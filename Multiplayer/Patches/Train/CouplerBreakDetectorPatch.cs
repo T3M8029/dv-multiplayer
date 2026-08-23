fb2dd9df8afcafcd9832aaaadc4d263699c261d1
Macka <andrew@andrewcraigmackenzie.com>
2026-08-23 17:03:57 +1000

    Prevent hoses from disconnecting in a de-sync

---
+++ Multiplayer/Patches/Train/CouplerBreakDetectorPatch.cs
@@ -0,0 +1,25 @@
+using DV.Simulation.Brake;
+using HarmonyLib;
+using Multiplayer.Components.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplayer.Patches.Train;
+
+[HarmonyPatch(typeof(CouplerBreakDetector))]
+public static class CouplerBreakDetectorPatch
+{
+    [HarmonyPatch(nameof(CouplerBreakDetector.OnJointBreak))]
+    [HarmonyPrefix]
+    private static bool OnCouplerBreak_Prefix(CouplerBreakDetector __instance, float breakForce)
+    {
+        if (NetworkLifecycle.Instance.IsHost())
+            return true;
+
+        Multiplayer.LogDebug(() => $"OnCouplerBreak_Prefix({__instance?.motherCoupler?.train?.ID}, breakForce: {breakForce})\r\n{new System.Diagnostics.StackTrace()}");
+        return false;
+    }
+}
